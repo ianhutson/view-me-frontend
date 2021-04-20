5222eeff4a711fbbb08e40184303974bbd9ce901
ianhutson <ih00209@georgiasouthern.edu>
2021-04-20 19:08:19 -0400

    finished oauth

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,12 +20,12 @@ class Login extends Component {
 render(){
     const clientId = process.env.REACT_APP_id;  
     const redirect = "http://localhost:3001/auth/twitch/callback"
-    const url = 'https://id.twitch.tv/oauth2/authorize?client_id=' + clientId + '&redirect_uri=' + redirect + '&response_type=code&scope=user:read:email';
+    const url = 'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=' + clientId + '&redirect_uri=' + redirect + '&scope=user:read:email';
     return (
     <div className="square">
       <image src="/img/logo.png" width="98" height="107" title="Logo" alt="Logo"></image>
       <p style={{color: 'black', fontSize: '35px'}}>Welcome to ViewMe! Please login. </p>
-      <a className="login_button" style={{width: '15%', margin: 'auto', textAlign: 'center'}} href={url}  > <image src="/img/twitch.png">Login with Twitch</image></a>
+      <a className="login_button" style={{width: '15%', margin: 'auto', textAlign: 'center'}} href={url} > <img src={process.env.PUBLIC_URL + "/twitch.png"}/>Login with Twitch</a>
     </div>)
     
     }
