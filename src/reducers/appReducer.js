ab1868b6e992e7de42f1905122f0b981e82cb622
ianhutson <ih00209@georgiasouthern.edu>
2021-05-04 14:57:48 -0400

    tweaked

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -9,21 +9,34 @@ export default function manageGame(state = {
     image: "",
     twitch_id: ""}},
     action) {
-    switch (action.type) {
-    
-      case "LOGIN_USER":
-        return {
-          current: action.user,
-          valid: true,
-          errors: []
-        }
-      case "INVALID_USER":
-        return {
-          current: {},
-          valid: false,
-          errors: {...action.errors}
-        }
-      default:
-        return state;
+      switch(action.type){
+        case "LOGIN_USER":
+          return {
+            current: action.user,
+            valid: true,
+            errors: []
+          }
+        case "INVALID_USER":
+          return {
+            current: {},
+            valid: false,
+            errors: {...action.errors}
+          }
+        case "START_AUTH":
+          return {
+            current: {...state.current},
+            valid: state.valid,
+            authCompleted: false,
+            errors: {...state.errors}
+          }
+        case "COMPLETE_AUTH":
+          return {
+            current: {...state.current},
+            valid: state.valid,
+            authCompleted: true,
+            errors: {...state.errors}
+          }
+        default:
+          return state
+      }
     }
-  }
