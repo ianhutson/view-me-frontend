9f64680a3f9bfa410b2a172c7b7bae94985d5d48
ianhutson <ih00209@georgiasouthern.edu>
2021-05-05 18:32:16 -0400

    frontend oauth nearly complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,47 @@ import React, { Component } from 'react';
 import Home from './components/Home'
 import About from './components/About'
 import Hearts from './components/Hearts'
+import Login from './components/Login'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import axios from 'axios'
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  };
+  componentDidMount() {
+    this.loginStatus()
+  }
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+   {withCredentials: true})    
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  };
+
+    handleLogin = (data) => {
+      this.setState({
+        isLoggedIn: true,
+        user: data.user
+      })
     }
+  handleLogout = () => {
+      this.setState({
+      isLoggedIn: false,
+      user: {}
+      })
+    }
+  
   render(){
     
   return (
@@ -18,13 +52,12 @@ class App extends Component{
         <Route path="/about"><About/></Route>
           <Route path="/hearts"><Hearts/></Route>
           <Route path='/'><Home/></Route>
-          
+          <Route path='/login'><Login/></Route>
         </Switch>
     </Router>
     </div>)
     }
 }
-// connect Auth to the redux state
 
 
 export default App;
